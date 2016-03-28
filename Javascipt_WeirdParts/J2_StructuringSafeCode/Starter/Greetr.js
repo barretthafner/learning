163809270503191787ba56be5a2dd1d4190b556d
Barrett Hafner <barrett@hafnerindustries.com>
2016-03-28 12:56:30 -0700

    start working on Greetr library

---
+++ Javascipt_WeirdParts/J2_StructuringSafeCode/Starter/Greetr.js
@@ -0,0 +1,20 @@
+(function(global, $) {
+
+  var Greetr = function(firstName, lastName, language) {
+    return new Greetr.init(firstName, lastName, language);
+  }
+
+  Greetr.init = function(firstName, lastName, language) {
+
+    var self = this;
+    self.firstName = firstName || '';
+    self.lastName = lastName || '';
+    self.language = language || 'en';
+
+  }
+
+  Greetr.init.prototype = Greetr.prototype;
+
+  global.Greetr = global.G$ = Greetr;
+
+}(window, jQuery));
