5d0917ea319236096e2ff74661e0b801d30f588e
Barrett Hafner <barrett@hafnerindustries.com>
2016-04-21 16:49:51 -0700

    start unit4 ajax

---
+++ fewd_001/unit4/lesson1/apps/app.js
@@ -0,0 +1,36 @@
+
+document.addEventListener('DOMContentLoaded', function(){
+
+  document.querySelector('#search-term').addEventListener('submit', function(){
+
+    var searchTerm = document.querySelector('#query').value;
+    getRequest(searchTerm, 'json');
+
+  }, false);
+}, false);
+
+
+function showResults(result) {
+  var content = '';
+
+    result.forEach(function(item){
+      content += '<p>' + item.Title + '</p>';
+    });
+
+  document.querySelector('#search-results').innerHTML = content;
+}
+
+
+function getRequest(searchTerm, responseType){
+
+  var params = {
+    s: searchTerm,
+    r: responseType,
+  };
+  url = 'http://www.omdbapi.com';
+
+
+  $.get(url, params, function(data){
+    showResults(data.Search);
+  }, params.r);
+}
