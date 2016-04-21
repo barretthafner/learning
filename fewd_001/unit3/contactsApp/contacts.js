f54500638b0544f6d125f0b767e46e1a8cbc4728
Barrett Hafner <barrett@hafnerindustries.com>
2016-04-21 12:41:28 -0700

    contactsApp: work on contact show functionality

--- fewd_001/unit3/contactsApp/contacts.js
+++ fewd_001/unit3/contactsApp/contacts.js
@@ -1,31 +1,58 @@
 
 $(document).ready(function(){
 
+  var config = {
+    selectors: {
+      firstName: '.first-name',
+      lastName: '.last-name',
+      phoneNumbers: '.phone-number',
+      addresses: '.addresses'
+    }
+  };
+
   var contactsDB = [];
 
   $('.contact-form button').on('click', function(event) {
     event.preventDefault();
 
     var contact = {
+      id: contactsDB.length,
       firstName: $('#firstName').val(),
       lastName: $('#lastName').val(),
-      phoneNumber: $('#phoneNumber').val(),
-      address: {
-        street: $('#addressStreet').val(),
-        city: $('#addressCity').val(),
-        state: $('#addressState').val()
-      }
+      phoneNumbers: [
+        $('#phoneNumber').val()
+      ],
+      addresses: [
+        {
+          street: $('#addressStreet').val(),
+          city: $('#addressCity').val(),
+          state: $('#addressState').val()
+        }
+      ]
     };
 
     contactsDB.push(contact);
-    console.log(contactsDB);
+
+    $('.contact-list').append('<li><a class="contact-link" data-id="' + contact.id + '" href="#">' + contact.firstName + ' ' + contact.lastName + '</a></li>');
 
 
-    $('.contact-list').append('<li><a href="#">' + contact.firstName + ' ' + contact.lastName + '</a></li>');
+    $('.contact-link').on('click', function(event){
+      event.preventDefault();
+      showContact($(event.target).attr('data-id'));
+    });
 
   });
 
-
-
+  function showContact(id) {
+    var contact = contactsDB[id];
+    $(config.selectors.firstName).text(contact.firstName);
+    $(config.selectors.lastName).text(contact.lastName);
+    contact.phoneNumbers.forEach(function(number, index){
+      $(config.selectors.phoneNumbers).html('').append('<li>Phone #' + (index + 1) + ': ' + number + '</li><hr/>');
+    });
+    contact.addresses.forEach(function(address, index){
+      $(config.selectors.addresses).html('').append('<li><p>Address #' + (index + 1) + ':</p><p>Street: ' + address.street + '</p><p>City: ' + address.city + '</p><p>State: ' + address.state + '</p></li><hr/>');
+    });
+  }
 
 });
