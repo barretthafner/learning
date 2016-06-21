c719c2d17e12541d6101c6e4e2793a7f629ec192
Barrett Hafner <barrett@hafnerindustries.com>
2016-06-21 11:50:04 -0700

    start testing coursework

---
+++ node/shopping-list/test/test-server.js
@@ -0,0 +1,17 @@
+
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+var server = require('../server.js');
+
+var should = chai.should();
+var app = server.app;
+var storage = server.storage;
+
+chai.use(chaiHttp);
+
+describe('Shopping List', function() {
+  it('should list items on get');
+  it('should add an item on post');
+  it('should edit an item on put');
+  it('should delete an item on delete');
+});
