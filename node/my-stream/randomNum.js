e71ead9be8899ce69b1725c947edeba03aece9bf
Barrett Hafner <barretth@gmail.com>
2016-06-23 22:16:25 +0000

    finish streams and callback coursework

---
+++ node/my-stream/randomNum.js
@@ -0,0 +1,20 @@
+var stream = require('stream');
+
+function RandomNum(options) {
+    stream.Readable.call(this, options);
+    this._curr = this._start.charCodeAt(0);
+};
+Alphabet.prototype = Object.create(stream.Readable.prototype);
+Alphabet.prototype.constructor = Alphabet;
+
+Alphabet.prototype._read = function() {
+    var letter = String.fromCharCode(this._curr);
+    var buf = new Buffer(letter, 'utf8');
+    this.push(buf);
+    this._curr++;
+    if (letter === this._end) {
+        this.push(null);
+    }
+};
+
+module.exports = Alphabet;
