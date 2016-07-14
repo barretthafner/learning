abe2fdfd5eaff835ab3fb3bc85be2477414d4b06
Barrett Hafner <barrett@hafnerindustries.com>
2016-07-14 00:32:07 -0700

    finish writing tests for react-testing project

--- react/react-testing/gallery.jsx
+++ react/react-testing/gallery.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 
 import Image from './image';
 
-export const Gallery = React.createClass({
+const Gallery = React.createClass({
   render() {
     const images = this.props.images.map(function(image, index) {
-        return <Image url={image.url} description={image.description} key={index}/>;
+      return <Image url={image.url} description={image.description} key={index}/>;
     });
 
     return (
-        <div className="gallery">{images}</div>
+      <div className="gallery">{images}</div>
     );
   }
 });
+
+module.exports = Gallery;
--- react/react-testing/test/test-image.jsx
+++ react/react-testing/test/test-image.jsx
@@ -3,6 +3,7 @@ import TestUtils from 'react-addons-test-utils';
 import { expect } from 'chai';
 
 import Image from '../image';
+import Gallery from '../gallery';
 
 describe('Image component', function() {
     it('Renders the image and description',  function() {
@@ -23,4 +24,38 @@ describe('Image component', function() {
         expect(p.type).to.equal('p');
         expect(p.props.children).to.equal(description);
     });
+});
+
+describe('Gallery component', function() {
+  it('Renders a gallery of images', function() {
+    const images = [
+      {
+        url: 'https://i.ytimg.com/vi/tntOCGkgt98/maxresdefault.jpg',
+        description: 'Maru in some paper'
+      },
+      {
+        url: 'https://s-media-cache-ak0.pinimg.com/236x/32/60/54/326054df712f51e8caf3819cee9b0253.jpg',
+        description: 'Cutest thing ever'
+      },
+      {
+        url: 'https://i.kinja-img.com/gawker-media/image/upload/s--GhnzKNzq--/c_fit,fl_progressive,q_80,w_636/18wggzydrz48njpg.jpg',
+        description: 'cute donuts'
+      },
+    ];
+
+    const renderer = TestUtils.createRenderer();
+    renderer.render(<Gallery images={images} />);
+    const result = renderer.getRenderOutput();
+
+    expect(result.props.className).to.equal('gallery');
+
+    const gallery = result.props.children;
+
+    expect(images.length).to.equal(3);
+
+    gallery.forEach(function(image, index) {
+      expect(image.type.displayName).to.equal('Image');
+      expect(Number(image.key)).to.equal(index);
+    });
+  });
 });
